9964112f55d03d2e4f428654379e9b8a80909d77
Witold Andreasik <witold.andreasik@gmail.com>
2020-06-28 17:09:50 +0200

    Updates

---
+++ precache-manifest.108ecc449ae3d197e1cadf2b007299da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b3773db82060e35a2540210e011c97",
+    "url": "/portfolioPage/index.html"
+  },
+  {
+    "revision": "2f08612eb045740658fb",
+    "url": "/portfolioPage/static/js/2.0cf9d9e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolioPage/static/js/2.0cf9d9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ecb342f21ab44b273aa",
+    "url": "/portfolioPage/static/js/main.7bcc8a1f.chunk.js"
+  },
+  {
+    "revision": "5cce959bcf5d5c1f8258",
+    "url": "/portfolioPage/static/js/runtime-main.674d379f.js"
+  },
+  {
+    "revision": "5821219dd374121cb2671d4bdc95c0e5",
+    "url": "/portfolioPage/static/media/problems.5821219d.png"
+  },
+  {
+    "revision": "ad6ef14b39bfe6b295b3579d1a094092",
+    "url": "/portfolioPage/static/media/redux-icon.ad6ef14b.png"
+  }
+]);
