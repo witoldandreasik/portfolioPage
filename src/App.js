f97c556d4181b3dbb8d64492d612bfb7590d6d22
Witold Andreasik <witold.andreasik@gmail.com>
2020-07-03 18:45:36 +0200

    add Suspense and lazy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 
-import HeaderSection from "./components/Header/HeaderSection";
-import Container from "./components/Container";
-import AboutMe from "./components/AboutMe/AboutMe";
-import Skills from "./components/Skills/Skills";
-import Projects from "./components/Projects/Projects";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./index.css.js";
 import theme from "./utils/theme";
+
+const HeaderSection = React.lazy(() =>
+  import("./components/Header/HeaderSection")
+);
+const Container = React.lazy(() => import("./components/Container"));
+const AboutMe = React.lazy(() => import("./components/AboutMe/AboutMe"));
+const Skills = React.lazy(() => import("./components/Skills/Skills"));
+const Projects = React.lazy(() => import("./components/Projects/Projects"));
 
 function App() {
   return (
     <>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
-        <Container>
-          <HeaderSection />
-          <AboutMe />
-          <Skills />
-          <Projects />
-        </Container>
+        {/* add loading indicator component in future*/}
+        <Suspense fallback={<div>loading...</div>}>
+          <Container>
+            <HeaderSection />
+            <AboutMe />
+            <Skills />
+            <Projects />
+          </Container>
+        </Suspense>
       </ThemeProvider>
     </>
   );
