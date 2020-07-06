851073243c1564956c9662ca1bf1490c8afd1ff4
Witold Andreasik <witold.andreasik@gmail.com>
2020-07-06 17:33:26 +0200

    change aboutme text

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -16,17 +16,12 @@ function AboutMe() {
       <div id="me-info">
         <h2>O mnie</h2>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
-          quibusdam repellat facilis ipsa. Consequatur molestias laboriosam
-          temporibus itaque aspernatur sapiente dignissimos fuga. Modi
-          consectetur laudantium minima nihil aliquam, dolor fuga. Lorem, ipsum
-          dolor sit amet consectetur adipisicing elit. Cumque quod quasi rerum.
-          Optio dolores ipsa voluptas ab, in repudiandae accusantium. Similique
-          assumenda voluptatem corporis laudantium, quisquam odit perspiciatis
-          animi numquam! Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Perferendis laborum cumque et ab voluptatem sed architecto
-          debitis perspiciatis qui nesciunt, inventore, nihil aut earum
-          reiciendis repellendus, voluptas obcaecati blanditiis! Saepe!
+          Pełen energii technik informatyk z licencją spawacza ubiegający się o
+          stanowisko Junior Frontend/React Developera. W pracy jestem
+          zorganizowany i zaangażowany w zlecone mi zadania. Uwielbiam pracę
+          zespołową i nowe wyzwania. Codziennie staram się rozwijać moje
+          umiejętności. W wolnym czasie czytam książki i literaturę
+          naukową(astronomia), gram w gry, oglądam seriale.
         </p>
       </div>
     </StyledAboutMe>
